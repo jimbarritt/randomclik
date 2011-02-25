f0870af4282cee01101d3156f159d47bc61d7ec8
Jim Barritt <jim@planet-ix.com>
2011-02-25 12:32:16 +0530

    [Jim, Sonar] Added a timout

--- src/main/web/js/randomclik.js
+++ src/main/web/js/randomclik.js
@@ -5,4 +5,12 @@ randomclik.go = function(url, webRoverId) {
 
     var webRoverIFrameElement = document.getElementById(webRoverId);
     webRoverIFrameElement.src = url;
+
+    setTimeout(randomclik.onTimeout, 1000);
 }
+
+
+randomclik.onTimeout = function() {
+    console.log("Going to look for another page...");
+}
+
